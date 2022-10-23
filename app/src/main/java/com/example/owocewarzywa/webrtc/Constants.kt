1cba4804b2149459a429962060f52db74f76e3e0
Bublelift <drjamesblond@hotmail.com>
2022-10-23 22:34:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/owocewarzywa/webrtc/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.owocewarzywa.webrtc
+
+class Constants {
+    companion object {
+        var isCallEnded: Boolean = false
+        var isInitiatedNow : Boolean = true
+    }
+}
---
+++ app/src/main/java/com/example/owocewarzywa/webrtc/LobbyActivity.kt
@@ -0,0 +1,59 @@
+package com.example.owocewarzywa.webrtc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import com.example.owocewarzywa.R
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+//import kotlinx.android.synthetic.main.chat_lobby_layout.*
+
+class LobbyActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
+    /******JK******/
+    val start_meeting = findViewById<MaterialButton>(R.id.start_meeting)
+    val meeting_id = findViewById<EditText>(R.id.meeting_id)
+    val join_meeting = findViewById<MaterialButton>(R.id.join_meeting)
+    /*****************/
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.chat_lobby_layout)
+        Constants.isInitiatedNow = true
+        Constants.isCallEnded = true
+        start_meeting.setOnClickListener {
+            if (meeting_id.text.toString().trim().isNullOrEmpty())
+                meeting_id.error = "Please enter meeting id"
+            else {
+                db.collection("calls")
+                        .document(meeting_id.text.toString())
+                        .get()
+                        .addOnSuccessListener {
+                            if (it["type"]=="OFFER" || it["type"]=="ANSWER" || it["type"]=="END_CALL") {
+                                meeting_id.error = "Please enter new meeting ID"
+                            } else {
+                                val intent = Intent(this@LobbyActivity, RTCActivity::class.java)
+                                intent.putExtra("meetingID",meeting_id.text.toString())
+                                intent.putExtra("isJoin",false)
+                                startActivity(intent)
+                            }
+                        }
+                        .addOnFailureListener {
+                            meeting_id.error = "Please enter new meeting ID"
+                        }
+            }
+        }
+        join_meeting.setOnClickListener {
+            if (meeting_id.text.toString().trim().isNullOrEmpty())
+                meeting_id.error = "Please enter meeting id"
+            else {
+                val intent = Intent(this@LobbyActivity, RTCActivity::class.java)
+                intent.putExtra("meetingID",meeting_id.text.toString())
+                intent.putExtra("isJoin",true)
+                startActivity(intent)
+            }
+        }
+    }
+}
